3135a600dc6def904341a610f13dfc4db569955d
Saahil Rane <ranesaahil@gmail.com>
2019-12-14 18:31:22 -0800

    Top Pane without images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule,
+  MatSliderModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatCardModule
+ } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+import { CardExampleComponent } from './card-example/card-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarTopComponent,
+    CardExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-example/card-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-example',
+  templateUrl: './card-example.component.html',
+  styleUrls: ['./card-example.component.css']
+})
+export class CardExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
